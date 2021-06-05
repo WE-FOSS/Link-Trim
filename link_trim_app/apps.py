7219b5ba6178b386c2d2b2bb63b79c90debea094
Tushar Verma <tushar.b.verma@accenture.com>
2021-06-05 21:53:11 +0530

    Partial Frontend Views

--- link_trim_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LinkTrimAppConfig(AppConfig):
-    name = 'link_trim_app'
--- link_trim_app/views.py
+++
@@ -1,6 +0,0 @@
-
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def home(request):
-    return render(request,'index.html')
